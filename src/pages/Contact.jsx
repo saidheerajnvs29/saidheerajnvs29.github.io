4a4a65d4be0856051d29482989d7a70bad242ac2
Venkata Sai Dheeraj Narayanabhatla <saidheerajnvs29@gmail.com>
2026-01-14 03:07:00 -0500

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,7 +4,7 @@ export default function Contact() {
   return (
     <section>
       <h1>Contact</h1>
-      <p className="lead">I'd love to hear from you</p>
+      <p className="lead">I'd love to hear from you!</p>
 
       <ul>
         <li>Email: <a href="mailto:saidheerajnvs1998@gmail.com">saidheerajnvs1998@gmail.com</a></li>
